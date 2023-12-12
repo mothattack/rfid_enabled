d68e4069c79d4e95671c7dc1729a087db5a95ceb
mothattack <timopie@gmail.com>
2023-12-12 15:15:00 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// JavaScript to show the modal after 30 seconds
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById('popupModal').style.display = 'block';
-    }, 30000); // 30000 milliseconds = 30 seconds
-};
+function showPopup() {
+    document.getElementById('popupModal').style.display = 'block';
+}
 
 document.getElementById('okButton').addEventListener('click', function() {
     document.getElementById('popupModal').style.display = 'none';
+    setTimeout(showPopup, 30000);
 });
+
+window.onload = function() {
+    setTimeout(showPopup, 30000);
+};
